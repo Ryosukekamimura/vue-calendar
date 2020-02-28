0a2953e98c75f003e1f72bca571e4bc04b933139
RyosukeKamimura <come1234560@gmail.com>
2020-02-28 16:30:16 +0900

    Calendar appを作成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import VueTextareaAutosize from 'vue-textarea-autosize';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
+
+
+Vue.use(VueTextareaAutosize)
 Vue.config.productionTip = false
 
+firebase.initializeApp({
+    apiKey: "AIzaSyD8lXuhfZWSwuLQtMzp_NTwirvbnU8fAx0",
+    authDomain: "vue-calendar-38910.firebaseapp.com",
+    databaseURL: "https://vue-calendar-38910.firebaseio.com",
+    projectId: "vue-calendar-38910",
+    storageBucket: "vue-calendar-38910.appspot.com",
+    messagingSenderId: "959292338580",
+    appId: "1:959292338580:web:526c26923c9f129873df81"
+});
+
+export const db = firebase.firestore();
+
+
+
 new Vue({
-  render: h => h(App),
+    vuetify,
+    render: h => h(App)
 }).$mount('#app')
